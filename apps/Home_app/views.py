ef738b1a41fc4d9ef9f494a95953bcfb334d1f16
Mahdiramazani <mahdiramazani1281@gmail.com>
2023-01-18 16:04:42 +0330

    add blog to Home.html

--- apps/Home_app/views.py
+++ apps/Home_app/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView,ListView
 from apps.Acount_app.models import User,Teacher
 from apps.Course_app.models import Courses,CoursesChild,Category
+from apps.Blog_App.models import BlogModel
 
 
 class HomeView(TemplateView):
@@ -13,10 +14,13 @@ class HomeView(TemplateView):
         coures=Courses.objects.all()
         teacher=Teacher.objects.filter(status=True)
         category=Category.objects.all()
+        last_blog=BlogModel.objects.all()[:3]
 
         context["coures"]=coures
         context["teacher"]=teacher
         context["category"]=category
+        context["object_list"]=last_blog
+
 
         return context
 
@@ -26,3 +30,5 @@ class HomeView(TemplateView):
 class FaqQuestionView(TemplateView):
 
     template_name = "Home_app/faq.html"
+
+
