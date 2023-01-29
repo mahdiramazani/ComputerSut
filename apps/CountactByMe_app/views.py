173f8a070cc5feeccb47c4d1655102a0615a92a5
Mahdiramazani <mahdiramazani1281@gmail.com>
2023-01-29 17:28:10 +0330

    send ajax form in countact_us and fix bug in countact_us

--- apps/CountactByMe_app/views.py
+++ apps/CountactByMe_app/views.py
@@ -1,28 +1,26 @@
 from django.shortcuts import render,redirect
 from django.views.generic import View,TemplateView
-from .forms import CountactUsForm
-
+from .models import ContactUs
+from django.http import JsonResponse
 class ContactView(View):
 
 
     def post(self,request):
-        form=CountactUsForm(data=request.POST)
 
+        name=request.POST.get("name")
+        phone=request.POST.get("phone")
+        body=request.POST.get("body")
 
-        if form.is_valid():
+        if name and body and phone:
+            ContactUs.objects.create(full_name=name,phone_number=phone,body=body)
+            message=f"{name} عزیز پیام شما ارسال شد"
+            return JsonResponse({"response":message})
 
-            form.save()
-
-
-            return redirect("CountactByMe_app:contact")
-
-
-        return render(request,"CountactByMe_app/contact.html",{"form":form})
+        return render(request,"CountactByMe_app/contact.html")
 
     def get(self,request):
-        form=CountactUsForm()
 
 
-        return render(request,"CountactByMe_app/contact.html",{"form":form})
+        return render(request,"CountactByMe_app/contact.html")
 
 
